9066d2e87ff7eab30475524f12e02f4ec5ac6509
Suhas Murthy <suhas.kvm@gmail.com>
2018-01-30 10:24:01 -0500

    http request handled

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'), function(req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
